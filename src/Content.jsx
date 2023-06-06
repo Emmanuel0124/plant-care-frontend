902388548d9c992b1f3dedd3a9d0f4947df16edd
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-06 13:06:23 -0500

    add create action for plants

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { PlantsIndex } from "./PlantsIndex";
+import { PlantsNew } from "./PlantsNew";
 
 export function Content() {
 
@@ -14,10 +15,20 @@ export function Content() {
     });
   };
 
+  const handleCreatePlant = (params, successCallback) => {
+    console.log("handleCreatePlant", params);
+    axios.post("http://localhost:3000/plants.json", params).then((response) => {
+      setPlants([...plants, response.data]);
+      successCallback();
+    });
+  };
+  
+
   useEffect(handleIndexPlants, []);
   
   return (
     <div>
+      <PlantsNew onCreatePlant={handleCreatePlant} />
       <PlantsIndex plants={plants} />
     </div>
   )
---
+++ src/PlantsNew.jsx
@@ -0,0 +1,28 @@
+export function PlantsNew(props) {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const params = new FormData(event.target);
+    props.onCreatePlant(params, () => event.target.reset());
+  };
+
+ return (
+  <div>
+    <h1>New Plant</h1>
+    <form onSubmit={handleSubmit}>
+      <div>
+        name: <input name="name" type="text" />
+      </div>
+      <div>
+        description: <input name="description" type="text" />
+      </div>
+      <div>
+        amount of sun: <input name="amount_of_sun" type="text" />
+      </div>
+      <div>
+        days to water: <input name="days_to_water" type="text" />
+      </div>
+      <button type="submit">Create Plant</button>
+    </form>
+  </div>
+ );
+}
