3da9884838c88bd0be69e636b025768412683b35
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-08 22:38:48 -0500

    add bootstrap for PlantsIndex

--- src/PlantsIndex.jsx
+++ src/PlantsIndex.jsx
@@ -1,16 +1,30 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
 export function PlantsIndex(props) {
   return (
-    <div>
-      <h1>all Plants</h1>
-      {props.plants.map((plant) => (
-        <div key={plant.id}>
-          <h2>{plant.name} </h2>
-          <p>Description: {plant.description}</p>
-          <p>Amount of sun: {plant.amount_of_sun}</p>
-          <p>Days to water: {plant.days_to_water}</p>
-          <button onClick={() => props.onShowPlant(plant)}>More Info</button>
-        </div>
-      ))}
+    <div className="container">
+      <h1>All Plants</h1>
+      <div className="row row-cols-1 row-cols-md-2 g-4">
+        {props.plants.map((plant) => (
+          <div key={plant.id} className="col">
+            <div className="card">
+              <div className="card-body">
+                <h2 className="card-title">{plant.name}</h2>
+                <p className="card-text">Description: {plant.description}</p>
+                <p className="card-text">Amount of sun: {plant.amount_of_sun}</p>
+                <p className="card-text">Days to water: {plant.days_to_water}</p>
+                <button
+                  className="btn btn-outline-success"
+                  onClick={() => props.onShowPlant(plant)}
+                >
+                  More Info
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
