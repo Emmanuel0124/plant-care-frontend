6a36e4c94be05d1c4577aad48ac430865d815be6
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-09 12:10:41 -0500

    add bootstrap navbar, edit logoutlink

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,24 @@
 import { LogoutLink } from "./LogoutLink";
-import { Link } from "react-router-dom";
+
 
 export function Header() {
   return(
     <header>
       <nav>
-       <Link to="/">Home</Link> | <Link to="/login">Login</Link> | <LogoutLink />
+      <ul className="nav justify-content-center">
+      <li className="nav-item">
+        <a className="nav-link disabled">Plant Reminder</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="/">Home</a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/Login">Login</a>
+      </li>
+      <li className="nav-item">
+        <LogoutLink />
+      </li>
+    </ul>
       </nav>
     </header>
   )
