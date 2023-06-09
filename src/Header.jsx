a6af88397787713a0a9e98554a1e8b5bbbec531a
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-09 16:04:37 -0500

    make icon component, disyplay home/login icon

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,26 +1,37 @@
 import { LogoutLink } from "./LogoutLink";
 import React from "react";
-import './header.css'; 
+import './header.css';
+import HomeIcon from "./HomeIcon";
+import LoginIcon from "./LoginIcon";
+// import LogoutIcon from "./LogoutIcon";
 
 export function Header() {
-  return(
+  return (
     <header>
       <nav>
-      <ul className="nav justify-content-center">
-      <li className="nav-item">
-        <a className="nav-link disabled">Plant Reminder</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" aria-current="page" href="/">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="/Login">Login</a>
-      </li>
-      <li className="nav-item">
-        <LogoutLink />
-      </li>
-    </ul>
+        <ul className="nav justify-content-center">
+          <li className="nav-item">
+            <a className="nav-link disabled">
+              Plant Reminder
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" aria-current="page" href="/">
+              <HomeIcon className="icon" />
+              Home
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="/Login">
+              <LoginIcon className="icon" />
+              Login
+            </a>
+          </li>
+          <li className="nav-item">
+            <LogoutLink />
+          </li>
+        </ul>
       </nav>
     </header>
-  )
+  );
 }
