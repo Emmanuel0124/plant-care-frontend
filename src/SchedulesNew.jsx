4f5532319c143de907e09943b7b93f2a56dcdb4d
EmmanuelBahena <mannybahena04@gmail.com>
2023-06-12 12:12:41 -0500

    edit schedulenew for css

--- src/SchedulesNew.jsx
+++ src/SchedulesNew.jsx
@@ -1,3 +1,5 @@
+import "./SchedulesNew.css";
+
 export function SchedulesNew(props) {
 
   const handleSubmit = (event) => {
@@ -9,17 +11,17 @@ export function SchedulesNew(props) {
   return (
     <div>
       <h1>New Schedule</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          plant Name: <input name="plant_id" type="text"/>
+      <form onSubmit={handleSubmit} className="schedule-form">
+        <div className="form-group">
+          plant Name: <input name="plant_id" type="text" className="form-input"/>
         </div>
-        <div>
-          watering start date: <input name="watering_start_date" type="text" />
+        <div className="form-group">
+          watering start date: <input name="watering_start_date" type="text" className="form-input" />
         </div>
-        <div>
-          User name: <input name="user_id"type="text" />
+        <div className="form-group">
+          User name: <input name="user_id"type="text" className="form-input" />
         </div>
-        <button type="submiy">Create schedule</button>
+        <button type="submit" className="submit-button">Create schedule</button>
       </form>
     </div>
   );
